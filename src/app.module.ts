ac9f972bebeb8c7e7e7516275eaf6701126c34b6
rodion <rodiontulub@gmail.com>
2023-04-21 19:49:42 +0300

    integrate type orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.model';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    AuthModule, 
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'nest',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
